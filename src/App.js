e2fb7efa8fa847c77db990aa6403b4cecfb372a0
Monil Kaneria <kaneriamonil@gmail.com>
2020-05-18 18:58:33 -0400

    made changes to app.js

--- src/App.js
+++ src/App.js
@@ -7,39 +7,39 @@ import './App.css';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
-// import {v4 as uuidv4} from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 import About from './components/pages/About';
-import axios from 'axios';
+// import axios from 'axios';
 
 class App extends Component {
 
-    state = {
-        todos: []
-    }
     // state = {
-    //     todos: [
-    //         {
-    //             id: uuidv4(),
-    //             title: 'qwerty',
-    //             completed: false
-    //         },
-    //         {
-    //             id: uuidv4(),
-    //             title: 'asdfgh',
-    //             completed: false
-    //         },
-    //         {
-    //             id: uuidv4(),
-    //             title: 'zxcvbn',
-    //             completed: false
-    //         }
-    //     ]
+    //     todos: []
     // }
+    state = {
+        todos: [
+            {
+                id: uuidv4(),
+                title: 'qwerty',
+                completed: false
+            },
+            {
+                id: uuidv4(),
+                title: 'asdfgh',
+                completed: false
+            },
+            {
+                id: uuidv4(),
+                title: 'zxcvbn',
+                completed: false
+            }
+        ]
+    }
 
-    componentDidMount() {
-        axios.get('http://jsonplaceholder.typicode.com/todos?_limit=5')
-            .then(res => this.setState({ todos: res.data }))
-    } 
+    // componentDidMount() {
+    //     axios.get('http://jsonplaceholder.typicode.com/todos?_limit=5')
+    //         .then(res => this.setState({ todos: res.data }))
+    // } 
 
     markComplete = (id) => {
         this.setState({todos: this.state.todos.map(todo => {
@@ -50,30 +50,30 @@ class App extends Component {
         })});
     }
 
-    deleteTodo = (id) => {
-        axios.delete(`http://jsonplaceholder.typicode.com/todos/${id}`)
-            .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }));
-        
-    }
     // deleteTodo = (id) => {
-    //     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+    //     axios.delete(`http://jsonplaceholder.typicode.com/todos/${id}`)
+    //         .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }));
+        
     // }
-
-    addTodo = (title) => {
-        axios.post('http://jsonplaceholder.typicode.com/todos', {
-            title,
-            completed: false
-        })
-            .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
+    deleteTodo = (id) => {
+        this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
     }
+
     // addTodo = (title) => {
-    //     const newTodo = {
-    //         id: uuidv4(),
+    //     axios.post('http://jsonplaceholder.typicode.com/todos', {
     //         title,
     //         completed: false
-    //     }
-    //     this.setState({ todos: [...this.state.todos, newTodo] });
+    //     })
+    //         .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
     // }
+    addTodo = (title) => {
+        const newTodo = {
+            id: uuidv4(),
+            title,
+            completed: false
+        }
+        this.setState({ todos: [...this.state.todos, newTodo] });
+    }
 
     render() {
         return (
